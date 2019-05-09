99246528b80e3ba0d7e1e13272737defd22f9ad9
leixueguang <lei008899@163.com>
2019-05-09 17:04:32 +0800

    20190508gs

---
+++ demo/fullpage/js/my.js
@@ -0,0 +1,25 @@
+window.onload =function(){
+	new fullpage('#fullpage', {
+		//options here
+		autoScrolling:true,
+		navigation:true,
+		scrollHorizontally: true
+	});
+
+	//methods
+	fullpage_api.setAllowScrolling(false);
+}
+
+
+// $(document).ready(function() {
+
+// 	$('#fullpage').fullpage({
+// 		//options here
+// 		autoScrolling:true,
+// 		navigation: true,
+// 		scrollHorizontally: true
+// 	});
+
+// 	//methods
+// 	$.fn.fullpage.setAllowScrolling(false);
+// });
